be7a1114478752231f52eb2f67ebceb7b2973b7e
Ramon.Ms <ramon.files.32@gmail.com>
2025-07-31 05:45:36 -0300

    update

---
+++ src/test/java/com/sloth/OnlyStudent/OnlyStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sloth.OnlyStudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
